bb6784604895c262b8d09cab793ab1830cdae4af
Sofian <sofian.slimani@edu.devinci.fr>
2021-03-12 16:53:12 +0100

    :construction: Header

--- resources/js/Pages/Customer/Index.vue
+++ resources/js/Pages/Customer/Index.vue
@@ -1,10 +1,26 @@
 <template>
+ <AppLayout>
+     <template #header>
+         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+             <p class="text-white">Clients</p>
+         </h2>
+     </template>
 
+     <div class="py-12">
+         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                 <welcome />
+             </div>
+         </div>
+     </div>
+ </AppLayout>
 </template>
 
 <script>
+import AppLayout from "@/Layouts/AppLayout";
 export default {
-name: "Index.vue"
+name: "Index.vue",
+    components: {AppLayout}
 }
 </script>
 
